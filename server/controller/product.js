d80cdc02ee99be5989e1fb00ca26b53e0122c4b0
Yashwant <77061466+YASH1730@users.noreply.github.com>
2022-11-11 22:34:01 +0530

    APIs added

--- server/controller/product.js
+++ server/controller/product.js
@@ -4,22 +4,64 @@ const product = require('../../database/models/product');
 // for getting the list of the product
 exports.getProducts = async (req, res) => {
 
-    product.find({}, {
-        product_title: 1,
-        product_image: 1,
-        featured_image: 1,
-        MRP: 1,
-        selling_price: 1,
-        discount_limit: 1,
-        SKU: 1,
-    }).skip(req.query.pageNumber > 0 ? (req.query.pageNumber - 1) * 10 : 0).limit(10)
-        .then((data) => {
+
+    //console.log(req.query)
+    let filter = undefined;
+
+    // for proceeding the filter if there is wrong json formate
+    if(req.query.filter)
+    {
+        try {filter = JSON.parse(req.query.filter)
+        }
+        catch {filter = undefined}
+    }
+
+    //console.log(filter)
+    // { pageNumber: '1', filter: '{"category_name": "/Table/i"  }' }
+    // { category_name: { '$regex': '/Table/i' } }
+
+    // final aggregation computing
+    product.aggregate([
+    {'$match' : filter || {} }, 
+    {'$group' : {'_id' : '$_id',
+                 'product_title': {'$first' : '$product_title'},
+                 'product_image': {'$first' : '$product_image'},
+                 'featured_image': {'$first' : '$featured_image'},
+                 'MRP': {'$first' : '$selling_price'},
+                 'selling_price': { '$first' :'$selling_price'},
+                 'discount_limit': {'$first' : '$discount_limit'},
+                 'SKU': {'$first' : '$SKU'},
+                 'category_name': {'$first' : '$category_name'},
+    }}, 
+    {'$sort' : {'SKU' : 1}},
+    {'$skip' : req.query.pageNumber > 0 ? (req.query.pageNumber - 1) * 10 : 0},
+    {'$limit' : 10}
+]).then((data) => {
             // data.map((row)=>console.log(row.SKU))
+            // console.log(data)
             return res.status(200).send(data);
         })
         .catch((err) => {
+            //console.log(err)
             return res.status(500).send({ message: 'Something went wrong !!!' })
         })
+
+//         {'$project' : {'selling_price': { 
+//             'selling_price' :  
+//             {
+//                 '$multiply' :
+//                         [ 
+//                         {
+//                             '$divide' : '$selling_price' !== 0 && '$discount_limit' !== 0 ?   
+//                                 ['$selling_price' , '$discount_limit' ] : [0]
+//                         },100
+//                         ]
+//             }
+        
+//     }
+// }
+// },
+
 
 }
 
