7c4bdfd5f07eca263dc5bbb26f34fe2e5c28de7d
Yashwant <77061466+YASH1730@users.noreply.github.com>
2023-01-13 13:38:31 +0530

    add :: maintenance page added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 // const port = process.env.PORT || 0; // for dynemically chaging the port
-const port = process.env.PORT || 8000; 
+const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser')
 const path = require('path')
 const mongo = require('./database/dbConfig');
@@ -18,31 +18,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', './src/public');
 
 //set up the view engine 
-app.set('view engine','pug')
-app.set('views','views')
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
 
 // set uploads as static
 
-app.use('/upload',express.static(path.join(__dirname, 'upload')));
+app.use('/upload', express.static(path.join(__dirname, 'upload')));
+
+// put the site on maintenance 
+app.use((req, res, next) => res.render('maintenance'));
 
 // requiring the routes
-app.use('/api/',require('./server/routes'))
+app.use('/api/', require('./server/routes'))
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
+app.use(express.static("frontEnd/build"));
 
-app.listen(port,()=>{
-        // console.log('Server is running at port',port);
-        // // console.log(app.address());
-    })
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontEnd', 'build', 'index.html'))
+})
+
+
+app.listen(port, () => {
+  // console.log('Server is running at port',port);
+  // // console.log(app.address());
+})
